5162aec7cb635883e7c90f5b87c6dc4158c39efc
Astrodelics <mikecurran0000@gmail.com>
2022-09-19 20:36:56 -0400

    9/19/22 End of Class

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ all_lines = my_file.readlines()
 for line in all_lines:
     upper_case_line = line.upper()
     upper_case_line = upper_case_line.strip()
-    print(upper_case_line)
+    course_and_name = upper_case_line.split(' : ')
+    print(course_and_name[0])
+    print(course_and_name[1])
 print("Those are the required COMP courses for the major")
